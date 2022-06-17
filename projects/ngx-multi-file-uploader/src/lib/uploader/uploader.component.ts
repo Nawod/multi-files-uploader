4e511242e5a7de30e6ee3156f63157f02ab36392
Nuwan1654 <dinusha@nsightsurgical.ai>
2022-06-17 11:12:09 +0000

    pass current file index to the server

--- projects/ngx-multi-file-uploader/src/lib/uploader/uploader.component.ts
+++ projects/ngx-multi-file-uploader/src/lib/uploader/uploader.component.ts
@@ -8,17 +8,17 @@ import { UploaderService } from '../uploader.service';
   styleUrls: ['./uploader.component.css']
 })
 export class UploaderComponent implements OnInit {
-  @Input() fileList : any[];
-  @Input() uploadRequestURL : any;
-  @Input() uploadURL : any;
-  @Input() chunkSize : number;
-  @Input() taskId : any;
+  @Input() fileList: any[];
+  @Input() uploadRequestURL: any;
+  @Input() uploadURL: any;
+  @Input() chunkSize: number;
+  @Input() taskId: any;
   @Input()
   uploadButtonEvent!: Subject<any>;
-  isUploading : boolean;
-  currentFileIndex : any;
-  lastUploadedFileIndex : any;
-  currentChunkIndex : any;
+  isUploading: boolean;
+  currentFileIndex: any;
+  lastUploadedFileIndex: any;
+  currentChunkIndex: any;
 
   @Output() progress = new EventEmitter<any>();
   @Output() logs = new EventEmitter<any>();
@@ -26,21 +26,21 @@ export class UploaderComponent implements OnInit {
   constructor(
     private _uploadService: UploaderService,
 
-  ) { 
+  ) {
     this.chunkSize = 5000000;
     this.isUploading = false;
     this.currentFileIndex = 0;
     this.currentChunkIndex = 0;
     this.taskId = ''
     this.fileList = [];
-    }
+  }
 
   ngOnInit(): void {
     console.log("uploader initailized")
 
-    this.uploadButtonEvent.subscribe(event=> {
+    this.uploadButtonEvent.subscribe(event => {
       this.isUploading = event;
-      if(event){
+      if (event) {
         console.log("Initial request for pID : ", this.taskId)
         this.requestNewUpload()
       }
@@ -50,8 +50,8 @@ export class UploaderComponent implements OnInit {
     this.uploadButtonEvent.unsubscribe()
   }
 
-   //make initial upload request
-   requestNewUpload () {
+  //make initial upload request
+  requestNewUpload() {
     const fileIndex = this.currentFileIndex;
     const file = this.fileList[fileIndex];
 
@@ -59,106 +59,107 @@ export class UploaderComponent implements OnInit {
     params.set("name", file.name);
     params.set("taskId", this.taskId);
     params.set("chunkSize", (this.chunkSize).toString());
+    params.set("currentFileIndex", (this.currentFileIndex).toString());
 
     const url =
-    this.uploadRequestURL + params.toString();
+      this.uploadRequestURL + params.toString();
 
     this._uploadService.uploadRequest(url).subscribe(
       (res) => {
-      console.log("from upload request - pID", this.taskId, res);
+        console.log("from upload request - pID", this.taskId, res);
 
-      const fileId = res.fileId;
-      const totalChunks = Math.ceil(this.fileList[fileIndex].size / this.chunkSize);
-      const existedChunks = res.existedChunks;
+        const fileId = res.fileId;
+        const totalChunks = Math.ceil(this.fileList[fileIndex].size / this.chunkSize);
+        const existedChunks = res.existedChunks;
 
-      if (res.exists) {
-        console.log("file", fileIndex +1 ," already exists with ", existedChunks, "chunks from ", totalChunks, "pID : ", this.taskId);
-        this.logs.emit(`file ${fileIndex + 1} already exists with ${existedChunks} chunks from ${totalChunks}`);
-        this.progress.emit({currentFileIndex:fileIndex, currentChunkIndex:this.currentChunkIndex,totalChunks:totalChunks })
+        if (res.exists) {
+          console.log("file", fileIndex + 1, " already exists with ", existedChunks, "chunks from ", totalChunks, "pID : ", this.taskId);
+          this.logs.emit(`file ${fileIndex + 1} already exists with ${existedChunks} chunks from ${totalChunks}`);
+          this.progress.emit({ currentFileIndex: fileIndex, currentChunkIndex: this.currentChunkIndex, totalChunks: totalChunks })
 
-        if (totalChunks === existedChunks) {
-          file.completed = true;
-          console.log("File ", fileIndex + 1, " Existed - pID : ", this.taskId);
-          this.logs.emit(`File ${fileIndex +1} Existed`);
+          if (totalChunks === existedChunks) {
+            file.completed = true;
+            console.log("File ", fileIndex + 1, " Existed - pID : ", this.taskId);
+            this.logs.emit(`File ${fileIndex + 1} Existed`);
 
-          this.currentFileIndex = fileIndex + 1;
-          this.currentChunkIndex = 0;
+            this.currentFileIndex = fileIndex + 1;
+            this.currentChunkIndex = 0;
 
-          if (fileIndex < this.fileList.length - 1 && this.isUploading) {
-            console.log("Trigerd from request:", this.currentFileIndex , "pID : ", this.taskId);
+            if (fileIndex < this.fileList.length - 1 && this.isUploading) {
+              console.log("Trigerd from request:", this.currentFileIndex, "pID : ", this.taskId);
 
-            this.requestNewUpload();
+              this.requestNewUpload();
+            }
+          } else {
+            this.currentChunkIndex = existedChunks;
+            this.readAndUploadCurrentChunk(fileId);
           }
         } else {
-          this.currentChunkIndex = existedChunks;
           this.readAndUploadCurrentChunk(fileId);
         }
-      } else {
-        this.readAndUploadCurrentChunk(fileId);
+      },
+      (error) => {
+        console.log("pID : ", this.taskId, " error from upload request: ", error);
+        this.logs.emit("Network Error!");
+        setTimeout(() => {
+          this.logs.emit("Network Error! Try to Reconnect...");
+          console.log("pID : ", this.taskId, " Try to reconnect from request!");
+          this.requestNewUpload();
+        }, 3000);
       }
-    },
-    (error) => {
-      console.log("pID : ", this.taskId," error from upload request: ", error);
-      this.logs.emit("Network Error!");
-      setTimeout(() => {
-        this.logs.emit("Network Error! Try to Reconnect...");
-        console.log("pID : ", this.taskId," Try to reconnect from request!");
-        this.requestNewUpload();
-      }, 3000);
-    }
     );
   };
 
-    //split the select file into chunks
-  readAndUploadCurrentChunk(fileId : any){
-      if (this.isUploading) {
-        const fileIndex = this.currentFileIndex;
-        const reader = new FileReader();
-        const file = this.fileList[fileIndex];
-        if (!file) {
-          return;
-        }
-  
-        const from = this.currentChunkIndex * this.chunkSize;
-        const to = from + this.chunkSize;
-        const blob = file.slice(from, to);
-        reader.onload = (e) => this.uploadChunk(e, fileId);
-        reader.readAsDataURL(blob);
+  //split the select file into chunks
+  readAndUploadCurrentChunk(fileId: any) {
+    if (this.isUploading) {
+      const fileIndex = this.currentFileIndex;
+      const reader = new FileReader();
+      const file = this.fileList[fileIndex];
+      if (!file) {
+        return;
       }
-    };
 
-      //upload the chunk file
-  uploadChunk(readerEvent:any, fileId:any){
+      const from = this.currentChunkIndex * this.chunkSize;
+      const to = from + this.chunkSize;
+      const blob = file.slice(from, to);
+      reader.onload = (e) => this.uploadChunk(e, fileId);
+      reader.readAsDataURL(blob);
+    }
+  };
+
+  //upload the chunk file
+  uploadChunk(readerEvent: any, fileId: any) {
     const fileIndex = this.currentFileIndex;
     const file = this.fileList[fileIndex];
     const data = (readerEvent.target.result);
     const totalChunks = Math.ceil(file.size / this.chunkSize)
 
-    this.logs.emit(`file ${fileIndex+1} uploading! Uploaded Chunks : ${this.currentChunkIndex} of ${totalChunks}`);
+    this.logs.emit(`file ${fileIndex + 1} uploading! Uploaded Chunks : ${this.currentChunkIndex} of ${totalChunks}`);
 
     const params = new URLSearchParams();
     params.set("name", file.name);
-    params.set("size", file.size);
     params.set("fileId", fileId);
     params.set("currentChunkIndex", this.currentChunkIndex);
     params.set("totalChunks", (totalChunks).toString());
     params.set("chunkSize", (this.chunkSize).toString());
+    params.set("currentFileIndex", (this.currentFileIndex).toString());
 
     const url = this.uploadURL + params.toString();
 
     if (this.isUploading) {
-      this._uploadService.upload(file,url,data).subscribe(
+      this._uploadService.upload(file, url, data).subscribe(
         (res) => {
           console.log("pID : ", this.taskId, " from upload ", res);
           const existedChunks = res.existedChunks;
-        this.progress.emit({currentFileIndex:this.currentFileIndex, currentChunkIndex:this.currentChunkIndex, totalChunks:totalChunks})
+          this.progress.emit({ currentFileIndex: this.currentFileIndex, currentChunkIndex: this.currentChunkIndex, totalChunks: totalChunks })
 
           if (res.completed) {
             file.completed = true;
             this.logs.emit(`File ${fileIndex + 1} upload completed!`);
-            console.log("pID : ", this.taskId ," File " ,fileIndex + 1, " - Uploaded");
+            console.log("pID : ", this.taskId, " File ", fileIndex + 1, " - Uploaded");
 
-            this.currentFileIndex = fileIndex +1
+            this.currentFileIndex = fileIndex + 1
             this.currentChunkIndex = 0;
 
             if (fileIndex < this.fileList.length - 1 && this.isUploading) {
@@ -173,7 +174,7 @@ export class UploaderComponent implements OnInit {
           }
         },
         (error) => {
-          console.log("pID : ", this.taskId," error from upload: ", error);
+          console.log("pID : ", this.taskId, " error from upload: ", error);
           this.logs.emit("Network Error!");
           setTimeout(() => {
             this.logs.emit("Network Error! Try to Reconnect...");
@@ -181,7 +182,7 @@ export class UploaderComponent implements OnInit {
             this.requestNewUpload();
           }, 3000);
         }
-        );
+      );
     }
   };
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,29 +16,29 @@ export class AppComponent implements OnInit {
   uploadURL = 'http://104.154.225.244:4001/upload?';
 
   _uploader: MultiFileUploader;
-  fileList : any[] = [];
+  fileList: any[] = [];
   logs: any = 'Chose files';
-  isUploading:boolean = false;
+  isUploading: boolean = false;
   attributeSet = {
-    uploadRequestURL : this.uploadRequestURL,
-    uploadURL : this.uploadURL,
-    chunkSize : this.chunkSize,
-    taskId : '1234a'
+    uploadRequestURL: this.uploadRequestURL,
+    uploadURL: this.uploadURL,
+    chunkSize: this.chunkSize,
+    taskId: '1234a'
   }
 
-  constructor (
+  constructor(
     private http: HttpClient
   ) {
     this._uploader = new MultiFileUploader(http)
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     //get upload status from the library
-    this._uploader.logObserve.subscribe(event=> {
+    this._uploader.logObserve.subscribe(event => {
       this.logs = event
     })
-    this._uploader.progressObserve.subscribe(event=>{
-      if(event.completed && event.currentFileIndex === this.fileList.length - 1){
+    this._uploader.progressObserve.subscribe(event => {
+      if (event.completed && event.currentFileIndex === this.fileList.length - 1) {
         this.isUploading = false;
         this._uploader.isUploading = false;
         this.fileList = []
@@ -53,21 +53,21 @@ export class AppComponent implements OnInit {
   }
 
   //Initiate values for uploader library and call the initail uploading request
-  uploadButtonHandler(){
-    if(this.fileList.length > 0){
-    this._uploader.isUploading = !this.isUploading;
-    this._uploader.attributeSet = this.attributeSet;
+  uploadButtonHandler() {
+    if (this.fileList.length > 0) {
+      this._uploader.isUploading = !this.isUploading;
+      this._uploader.attributeSet = this.attributeSet;
 
-    this.isUploading = !this.isUploading;
+      this.isUploading = !this.isUploading;
 
-    if(this.isUploading){
-    this._uploader.requestNewUpload()
-    }
+      if (this.isUploading) {
+        this._uploader.requestNewUpload()
+      }
     }
   }
 
   //append the selected files to file list and pass it to the library
-  addFile(event:any){
+  addFile(event: any) {
     this.fileList = [...this.fileList, ...event.target.files]
     this._uploader.fileList = this.fileList
   }
